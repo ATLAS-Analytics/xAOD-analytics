d1b1cceb80dd5b780942fb6dac45e6772eff5f8d
Ilija Vukotic <ivukotic@cern.ch>
2015-10-26 10:18:53 -0500

    moving things around

--- import/run.sh
+++ import/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# this script is called from a cron once a day after script importing the new data
+# this script will be called from a cron once a day after script importing the new data
 # it runs a pig script to index the new data in ES.
 
 kinit ivukotic@CERN.CH -k -t /afs/cern.ch/user/i/ivukotic/ivukotic.keytab
 
-cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/xAOD/IO
+cd /afs/cern.ch/user/i/ivukotic/xAOD-analytics/import
 DateToProcess=$(date +%Y-%m-%d -d "-4day")
 echo "Indexing... CL "${DateToProcess}
 pig -4 log4j.properties -f HeatMapOffGridIndexing.pig -param INPD=${DateToProcess}
--- pig/load.sh
+++ pig/load.sh
@@ -1,1 +1,1 @@
-export CLASSPATH=/afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/xAOD/IO/json.jar
+export CLASSPATH=/afs/cern.ch/user/i/ivukotic/xAOD-analytics/libs/json.jar;/afs/cern.ch/user/i/ivukotic/xAOD-analytics/xAOD-parser/target/xAODparser-0.0.1.jar
