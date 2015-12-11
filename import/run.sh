e1c0754423312302a90366d40e08727ef0a362b0
Ilija Vukotic <ivukotic@cern.ch>
2015-12-10 22:51:37 -0600

    reorg

--- import/run.sh
+++ import/run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# this script will be called from a cron once a day after script importing the new data
-# it runs a pig script to index the new data in ES.
-
 kinit ivukotic@CERN.CH -k -t /afs/cern.ch/user/i/ivukotic/ivukotic.keytab
 
 cd /afs/cern.ch/user/i/ivukotic/xAOD-analytics/import
-DateToProcess=$(date +%Y-%m-%d -d "-4day")
-echo "Indexing... CL "${DateToProcess}
-pig -4 log4j.properties -f HeatMapOffGridIndexing.pig -param INPD=${DateToProcess}
+DateToProcess=$(date +%Y-%m-%d -d "-1day")
+#DateToProcess=2015-11-25
+echo "Indexing...  "${DateToProcess}
+pig -4 log4j.properties -f JobIndexer.pig -param INPD=${DateToProcess}
 
-
-
-
+echo "Done. Starting the other indexer..."
+pig -4 log4j.properties -f accessesIndexer.pig -param INPD=${DateToProcess}
+echo "pig code finished."
+rm -f heatmap.csv
+hdfs dfs -getmerge heatmap.csv heatmap.csv
+python indexer.py ${DateToProcess}
+echo "upload finished."
