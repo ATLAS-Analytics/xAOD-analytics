e679cff028e25bd6029b3071699e3d19f9214d07
Ilija Vukotic <ivukotic@uchicago.edu>
2018-01-18 21:16:46 +0100

    add run for container.

---
+++ import/run_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DateToProcess=$(date +%Y-%m-%d -d "-1day")
+IND=$(date +%Y.%m -d "-1day")
+echo "Indexing...  "${DateToProcess}
+pig -4 log4j.properties -f JobIndexer.pig -param INPD=${DateToProcess} -param INDE=${IND}
+echo "pig code finished."
+
+echo "Done. Starting the other indexer..."
+rm -f heatmap.csv
+hdfs dfs -getmerge heatmap.csv heatmap.csv
+python indexer.py ${IND}
+echo "upload finished."
