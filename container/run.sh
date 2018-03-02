34a74dc7fc884457663a08cad5dce5f78ab6f625
Ilija Vukotic <ivukotic@cern.ch>
2018-03-02 23:20:52 +0100

    adding return code checks

--- container/run.sh
+++ container/run.sh
@@ -4,10 +4,22 @@ DateToProcess=$(date +%Y-%m-%d -d "-1day")
 IND=$(date +%Y.%m -d "-1day")
 echo "Indexing...  "${DateToProcess}
 pig -4 log4j.properties -f JobIndexer.pig -param INPD=${DateToProcess} -param INDE=${IND}
+rc=$?; if [[ $rc != 0 ]]; then 
+    echo "problem with pig indexer. Exiting."
+    exit $rc
+fi
 echo "pig code finished."
 
 echo "Done. Starting the other indexer..."
 rm -f heatmap.csv
 hdfs dfs -getmerge heatmap.csv heatmap.csv
+rc=$?; if [[ $rc != 0 ]]; then 
+    echo "problem with getmerge. Exiting."
+    exit $rc
+fi
 python indexer.py ${IND}
+rc=$?; if [[ $rc != 0 ]]; then 
+    echo "problem with python indexer. Exiting."
+    exit $rc
+fi
 echo "upload finished."
