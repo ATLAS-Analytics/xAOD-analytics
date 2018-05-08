4aaac17a05e6f3ae7fb2d4b19bf0aedf1b1236b8
Ilija Vukotic <ivukotic@cern.ch>
2018-05-08 12:45:43 -0500

    bf

--- container/fix.sh
+++ container/fix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-Ind="2018-01"
+IND="2018-01"
 for i in $(seq -f "%02g" 1 31)
 do
 
-DateToProcess="$Ind-$i"
+DateToProcess="$IND-$i"
 echo "Job Indexing...  "${DateToProcess}
 
 pig -4 log4j.properties -f JobIndexer.pig -param INPD=${DateToProcess}
